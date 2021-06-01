48fb08d61d823005d3ffdd9c65eac151005c029b
Kenneth Hurley <klhurley@yahoo.com>
2021-05-31 18:18:04 -0700

    making relative git repos for move to github eventually and adding TokenContracts submodule

--- TokenContracts/ICO/migrations/2_deploy.js
+++
@@ -1,8 +0,0 @@
-const ICOContract = artifacts.require('ICOContract');
-
-module.exports = async function (deployer, network, accounts) {
-    let _gnusTokenAddress = "0x75D9E8790128eb333A2632f8cd7C631d3c03564a";
-    let _initLimits = [12500, 12500, 12500, 12500];
-    let _initRates = [1000, 800, 640, 512];
-    await deployer.deploy(ICOContract, _gnusTokenAddress, _initLimits, _initRates);
-};
